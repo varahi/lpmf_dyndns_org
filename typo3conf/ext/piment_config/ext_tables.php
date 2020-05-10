092ecb35e54d4e277819bdd35673e8c87f6ece8c
Dmitry Vasilev <dmitry@t3dev.ru>
2020-05-10 21:52:25 +0300

    Feature #407 - EXT:piment_config - re-configure TSConfig

--- typo3conf/ext/piment_config/ext_tables.php
+++ typo3conf/ext/piment_config/ext_tables.php
@@ -4,3 +4,19 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Piment Config');
+
+/**
+ * Page TS Config
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:piment_config/Configuration/TSConfig/page.tsconfig">');
+
+/**
+ * Add some basic User TS Config
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:piment_config/Configuration/TSConfig/user.tsconfig">');
+
+/**
+ * Add rte_ckeditor custom config
+ */
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['custom'] = 'EXT:piment_config/Configuration/RTE/Custom.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['pimentrouge'] = 'EXT:piment_config/Configuration/RTE/Default.yaml';
